edb3ae420b67a4d6f6b6b6935fb8edb943c07eb6
IdionKim <ksundong@gmail.com>
2020-03-23 21:26:46 +0900

    [#4] Feat: 프로젝트 시작
     - Spring Initializer 사용
     - README.md 수정
     - gitignore 설정

---
+++ BE/src/test/java/com/codesquad/team1/signup/SignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.team1.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
