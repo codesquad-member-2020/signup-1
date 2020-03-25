e90cfcf6c40b556c36f8a070d7e0301f9e1d79d0
최성익 <tjddlr92@gmail.com>
2020-03-26 02:05:41 +0900

    [#21] Feat: 이메일, 핸드폰 검증 추가
    - 생년월일 진행중
    - 알고리즘 제작 필요. 일단은 간단하게 만들 예정
    - 직접적으로 텍스트가 변경되는 부분은 없음. 함수형으로 제작할 예정.

--- FE/src/modules/util.js
+++ FE/src/modules/util.js
@@ -7,3 +7,17 @@ export const _qa = str => {
 };
 
 export const pipe = (...fns) => value => fns.reduce((acc, fn) => fn(acc), value);
+
+export const daysInMonth = num => {
+  switch (num) {
+    case "2":
+      return 28;
+    case "4":
+    case "6":
+    case "9":
+    case "11":
+      return 30;
+    default:
+      return 31;
+  }
+};
