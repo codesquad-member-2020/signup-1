1d86037972e3e5edd8be638a1a2c7a3e5e2f4615
Jay <beginin15@gmail.com>
2020-03-25 12:13:03 +0900

    [#6] Refactor: 중복 id 쿼리문 피드백 반영
    - count를 이용하여 개수를 리턴하도록 변경. 그에 따라 Optional도 불피요해져서 제거함.

--- BE/src/main/java/com/codesquad/team1/signup/repository/UserRepository.java
+++ BE/src/main/java/com/codesquad/team1/signup/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.codesquad.team1.signup.repository;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends CrudRepository<User, Integer> {
-    @Query("select user_id from USERS where user_id = :userId")
-    Optional<String> findByUserId(String userId);
+    @Query("select count(*) from USERS where user_id = :userId")
+    long findByUserId(String userId);
 }
