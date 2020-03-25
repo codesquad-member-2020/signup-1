c30f03439659a0445ce6f2bb1efa94c125e99ace
Jay <beginin15@gmail.com>
2020-03-26 01:58:25 +0900

    [#10] Style: UserRepository 쿼리 메소드명 변경
    - 중복 체크에 사용되는 메소드의 이름을 countBy-로 시작하도록 변경하여 findBy-와 구분.

--- BE/src/main/java/com/codesquad/team1/signup/repository/UserRepository.java
+++ BE/src/main/java/com/codesquad/team1/signup/repository/UserRepository.java
@@ -3,14 +3,19 @@ package com.codesquad.team1.signup.repository;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("select count(*) from USERS where binary user_id = :userId")
-    long findByUserId(String userId);
+    long countByUserId(String userId);
 
     @Query("select count(*) from USERS where binary email = :email")
-    long findByEmail(String email);
+    long countByEmail(String email);
 
     @Query("select count(*) from USERS where binary phone_number = :phoneNumber")
-    long findByPhoneNumber(String phoneNumber);
+    long countByPhoneNumber(String phoneNumber);
+
+    @Query("select * from USERS where binary user_id = :userId")
+    Optional<User> findByUserId(String userId);
 }
