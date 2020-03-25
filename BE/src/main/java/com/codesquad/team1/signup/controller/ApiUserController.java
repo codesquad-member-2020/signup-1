9f47b66fa6124bb69ddf2a1c17e7b20332d6a2c5
IdionKim <ksundong@gmail.com>
2020-03-25 21:02:44 +0900

    [#78] Feat: User 객체 자체에서 validate를 수행하는 코드 작성
     - 사용하지 않는 Optional import를 제거하였습니다. Fixed #62 Issue.
     - validate를 User 객체에서 직접 수행하는, 객체지향적인 접근을 시도하였습니다.
     - Close #78 Issue.

--- BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
+++ BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/users")
 public class ApiUserController {
