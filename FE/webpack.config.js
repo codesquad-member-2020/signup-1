2978b97789317239e86c4433dec70043e51a8f24
최성익 <tjddlr92@gmail.com>
2020-03-25 17:27:07 +0900

    [#70] Chore: 웹팩 개발환경 세팅
    - 여러 html 파일을 각각 번들링 할 수 있도록 수정
    - 참고: http://choiyb2.tistory.com/96

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -4,17 +4,26 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: ["./src/main.js", "./scss/main.scss"],
+  entry: { index: "./src/signup.js", signin: "./src/signin.js", home: "./src/home.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "css/style.css" }),
+    new MiniCssExtractPlugin({ filename: "css/[name].css", chunkFilename: "css/[id].css" }),
     new HtmlWebpackPlugin({
-      minify: {
-        collapseWhitespace: true,
-      },
+      filename: "index.html",
+      chunks: ["index"],
+      template: "./src/signup.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "signin.html",
+      chunks: ["signin"],
+      template: "./src/signin.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "home.html",
+      chunks: ["home"],
       template: "./src/home.html",
     }),
   ],
