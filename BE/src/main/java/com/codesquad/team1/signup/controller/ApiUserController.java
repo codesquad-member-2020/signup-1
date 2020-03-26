f7482d10f8671f0037bf38df1f576d1f54c897e4
IdionKim <ksundong@gmail.com>
2020-03-26 23:38:42 +0900

    [#88] Feat: 회원가입시 관심사 데이터 추가
     - 관심사 정보를 입력받을 수 있도록 하였습니다.
     - User 객체 비교시 this 키워드를 사용하도록 하였습니다.
     - User 객체 hash 사용시 잘못된 정보를 hash 하는 것을 수정하였습니다.
     - Close #88 Issue.

--- BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
+++ BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
@@ -7,6 +7,7 @@ import com.codesquad.team1.signup.repository.UserRepository;
 import com.codesquad.team1.signup.response.ValidationResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -51,6 +52,7 @@ public class ApiUserController {
     }
 
     @PostMapping("/create")
+    @Transactional
     public User createUser(@RequestBody User user) {
         if (user.validate()) {
             return userRepository.save(user);
---
+++ BE/src/main/java/com/codesquad/team1/signup/repository/InterestRepository.java
@@ -0,0 +1,5 @@
+package com.codesquad.team1.signup.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InterestRepository extends CrudRepository<Interest, Integer> {}
