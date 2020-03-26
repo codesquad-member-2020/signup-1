9f744a38b29c13db574504e14f82eea57a66ee3b
Jay <beginin15@gmail.com>
2020-03-27 03:11:23 +0900

    [#107] Style: 불필요한 import 제거

--- BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
+++ BE/src/main/java/com/codesquad/team1/signup/controller/ApiUserController.java
@@ -9,7 +9,6 @@ import com.codesquad.team1.signup.response.ValidationResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
