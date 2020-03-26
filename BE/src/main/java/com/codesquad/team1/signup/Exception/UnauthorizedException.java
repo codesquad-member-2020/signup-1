37cd693d1f69cff44f349184880d726cb81601cb
Jay <beginin15@gmail.com>
2020-03-27 02:58:46 +0900

    [#107] Refactor: 에러 발생 시 응답 객체 구현
    - ErrorResponse 객체에 에러 메세지와 http 상태 코드를 저장.
    - 현재 하나의 컨트롤러 밖에 존재하지 않기 때문에 해당 컨트롤러 안에서@ExceptionHandler를 이용해서 예외 상황을 처리하도록 구현함.

--- BE/src/main/java/com/codesquad/team1/signup/Exception/UnauthorizedException.java
+++ BE/src/main/java/com/codesquad/team1/signup/Exception/UnauthorizedException.java
@@ -1,9 +1,5 @@
 package com.codesquad.team1.signup.Exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException(String message) {
