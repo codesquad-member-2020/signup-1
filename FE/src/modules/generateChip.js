eeda23affcbec1a922842775771d6a7e12285be9
최성익 <tjddlr92@gmail.com>
2020-03-27 01:55:15 +0900

    [#24] Chore: 비동기 테스트
    - 비동기 테스트 위해 번들링 파일 우선 하나로.
    - 바벨-폴리필에서 에러가 발생한다. async await가 제대로 먹지 않아 설치했는데, 이걸 설치하면 여러개 번들링이 안된다.

--- FE/src/modules/generateChip.js
+++ FE/src/modules/generateChip.js
@@ -1,16 +1,35 @@
-import inputFields from "./fields.js";
+import { pipe } from "./util.js";
+import { inputFields } from "./fields.js";
+import { COMMA } from "./constants.js";
 
 export const chip = {
   chips: new Set(),
   getChip: () => this.chips,
-  addChip: str => this.chips.set(str),
+  addChip: str => this.chips.add(str),
   deleteChip: str => this.chips.delete(str),
   getSizeofChip: () => this.chips.size,
   clearChip: () => this.chips.clear(),
 };
 
+const hasComma = str => {
+  const restWord = str.slice(0, -1);
+  return restWord.includes(COMMA);
+};
+
+const splitString = elem => elem.value.split(COMMA);
+const arrayToString = arr => arr.reduce((acc, cur) => acc.concat(cur), "");
+const removeTargetString = pipe(splitString, arrayToString);
+
 export const generateChip = event => {
   const interest = inputFields.interest.inputElement;
-  // if (event.target === interest) {
-  // }
+  if (event.target === interest) {
+    if (event.data === COMMA) {
+      if (hasComma(interest.value)) {
+        interest.value = removeTargetString(interest);
+        return;
+      }
+      chip.addChip(interest.value);
+      console.log(chip.getChip);
+    }
+  }
 };
