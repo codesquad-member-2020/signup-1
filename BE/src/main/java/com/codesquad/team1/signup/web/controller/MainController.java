1f322e983affa2952808905f7ff3a9db5375b205
IdionKim <ksundong@gmail.com>
2020-03-26 19:02:22 +0900

    [#75] Feat: FE와 BE 연동
     - FE의 dist 파일을 그대로 static에 옮겨와서 사용했습니다.
     - template에 넣어주고 싶은데 넣으면 css가 안읽혀서 일단 현 상태로 진행하려고 합니다.
     - Close #75 Issue

---
+++ BE/src/main/java/com/codesquad/team1/signup/web/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.codesquad.team1.signup.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String goMainPage() {
+        return "redirect:index.html";
+    }
+}
