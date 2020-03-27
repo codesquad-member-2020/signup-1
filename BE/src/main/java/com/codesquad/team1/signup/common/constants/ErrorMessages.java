fd6405f6fe20e3783ccf07c415073f3bc146af25
Jay <beginin15@gmail.com>
2020-03-27 14:59:22 +0900

    [#109] Refactor: 에러 메세지를 모아둔 ErrorMessages 클래스 정의

---
+++ BE/src/main/java/com/codesquad/team1/signup/common/constants/ErrorMessages.java
@@ -0,0 +1,10 @@
+package com.codesquad.team1.signup.common.constants;
+
+public class ErrorMessages {
+
+    public static final String FORBIDDEN = "접근 권한이 없습니다.";
+    public static final String UNAUTHORIZED = "로그인이 필요합니다.";
+    public static final String INVALID_PARAMETER = "Validation을 통과하지 못한 값 입니다.";
+
+    private ErrorMessages() {}
+}
