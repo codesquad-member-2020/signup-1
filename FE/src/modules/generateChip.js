ceb6e2fe34817aee66eec4deaf438e1bb5acc9ec
최성익 <tjddlr92@gmail.com>
2020-03-27 15:23:21 +0900

    [#24] 관심사 칩 기능 구현 (마우스 클릭 제외)
    - Set 을 사용해 관심사 태그가 생기고 지워지는 기능 구현
    - focus 별도로 구현

--- FE/src/modules/generateChip.js
+++ FE/src/modules/generateChip.js
@@ -1,35 +1,53 @@
-import { pipe } from "./util.js";
+import { _q, _qa, pipe, addClass } from "./util.js";
 import { inputFields } from "./fields.js";
-import { COMMA } from "./constants.js";
+import { KEY_CODE_BACKSPACE, KEY_CODE_COMMA, COMMA, CHIP_ELEMENT_CLASS_NAME, CHIP_ELEMENT_HTML } from "./constants.js";
 
-export const chip = {
-  chips: new Set(),
-  getChip: () => this.chips,
-  addChip: str => this.chips.add(str),
-  deleteChip: str => this.chips.delete(str),
-  getSizeofChip: () => this.chips.size,
-  clearChip: () => this.chips.clear(),
+export const chips = new Set();
+const getChip = () => chips;
+const addChip = str => chips.add(str);
+const deleteChip = str => chips.delete(str);
+const getSizeofChip = () => chips.size;
+const clearChip = () => chips.clear();
+const getLastChip = set => {
+  let value;
+  for (value of set);
+  return value;
 };
 
-const hasComma = str => {
-  const restWord = str.slice(0, -1);
-  return restWord.includes(COMMA);
-};
-
-const splitString = elem => elem.value.split(COMMA);
+const hasComma = str => str.includes(COMMA);
+const splitStringbyComma = str => str.split(COMMA);
 const arrayToString = arr => arr.reduce((acc, cur) => acc.concat(cur), "");
-const removeTargetString = pipe(splitString, arrayToString);
+const removeComma = pipe(splitStringbyComma, arrayToString);
+
+export const resetChips = () => {
+  const chipElement = _qa(`.${CHIP_ELEMENT_CLASS_NAME.div}`);
+  if (chipElement) chipElement.forEach(eachChip => eachChip.remove());
+};
+
+export const renderChips = () => {
+  resetChips();
+  const reverseChips = new Set([...chips].reverse());
+  const interestElement = _q(`.${CHIP_ELEMENT_CLASS_NAME.wrap}`);
+  reverseChips.forEach(chipValue => {
+    interestElement.insertAdjacentHTML("afterbegin", CHIP_ELEMENT_HTML(chipValue));
+  });
+};
 
 export const generateChip = event => {
   const interest = inputFields.interest.inputElement;
   if (event.target === interest) {
-    if (event.data === COMMA) {
+    if (event.keyCode === KEY_CODE_COMMA) {
+      event.preventDefault();
       if (hasComma(interest.value)) {
-        interest.value = removeTargetString(interest);
-        return;
+        interest.value = removeComma(interest.value);
       }
-      chip.addChip(interest.value);
-      console.log(chip.getChip);
+      addChip(interest.value);
+      interest.value = "";
+      renderChips();
+    }
+    if (event.keyCode === KEY_CODE_BACKSPACE && interest.value === "") {
+      deleteChip(getLastChip(chips));
+      renderChips();
     }
   }
 };
--- FE/src/modules/main.js
+++ FE/src/modules/main.js
@@ -1,18 +1,27 @@
-import { _q } from "./util.js";
+import { _q, addClass, removeClass } from "./util.js";
+import { FORM_ID, CHIP_ELEMENT_CLASS_NAME } from "./constants.js";
 import { validateInputForms, validateSelectForms, validateDuplicateId, preventKeypressExceptNum } from "./validation.js";
+import { generateChip } from "./generateChip.js";
 import userData from "./userData.js";
 
-const signupForm = _q("form");
-const buttons = _q(".btn-wrap");
+const signupForm = _q(FORM_ID.form);
+const interest = _q(FORM_ID.interest);
+const buttons = _q(FORM_ID.buttons);
 
-signupForm.addEventListener("input", event => {
+signupForm.addEventListener("keyup", event => {
   validateInputForms(event);
+  generateChip(event);
 });
+
 signupForm.addEventListener("change", event => {
   validateDuplicateId(event);
   validateSelectForms(event);
 });
+
 signupForm.addEventListener("keypress", event => preventKeypressExceptNum(event));
+
+interest.addEventListener("focus", () => addClass(_q(`.${CHIP_ELEMENT_CLASS_NAME.wrap}`), "focus"));
+interest.addEventListener("blur", () => removeClass(_q(`.${CHIP_ELEMENT_CLASS_NAME.wrap}`), "focus"));
 
 buttons.addEventListener("click", event => {
   event.preventDefault();
